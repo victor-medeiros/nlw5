08154d3d55377de9c7fd0f455ffef2102f5c1bfe
victor-medeiros <victor.msilva@bandtec.com>
2021-04-21 10:29:54 -0300

    User entity, repository, service and controller creation

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Request, Response, Router } from "express";
 import { SettingController } from "./controller/SettingController";
+import { UserController } from "./controller/UserController";
 
 
 const route = Router();
 
 const settingController = new SettingController();
+const userController = new UserController();
 
 route.post("/settings", settingController.create);
+route.post("/users", userController.create);
 
 export { route };
---
+++ src/service/UserService.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
+
+interface UserCreation {
+  email: string
+}
+
+class UserService {
+  async create({ email }: UserCreation){
+    const userRepository = getCustomRepository(UserRepository);
+
+    const userExists = await userRepository.findOne({ email });
+
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = userRepository.create({ email });
+
+    await userRepository.save(user);
+    return user;
+  }
+}
+
+export { UserService }
