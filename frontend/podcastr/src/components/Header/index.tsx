2dc37df38eda61b62a9132ac56f5271e035c3d19
victor-medeiros <victor.msilva@bandtec.com>
2021-06-14 19:03:30 -0300

    Criação do componente Header

--- frontend/podcastr/src/components/Header/index.tsx
+++ frontend/podcastr/src/components/Header/index.tsx
@@ -1,7 +1,18 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/esm/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
 export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  })
   return (
-    <header>
+    <header className={styles.headerContainer}>
       <img src="logo.svg" alt="Podcastr" />
+
+      <p>O melhor para você ouvir, sempre.</p>
+      <span>{currentDate}</span>
     </header>
   );
 }
