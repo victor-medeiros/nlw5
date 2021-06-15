db90d62775b020943d1f0f4d3135f1db6a8f3aa6
victor-medeiros <victor.msilva@bandtec.com>
2021-06-14 22:36:27 -0300

    Mudanças

--- frontend/podcastr/src/pages/index.tsx
+++ frontend/podcastr/src/pages/index.tsx
@@ -1,5 +1,19 @@
 import { Header } from "../components/Header";
 
-export default function Home() {
-  return <div>Index</div>
+export default function Home(props) {
+  console.log(props.episodes)
+  return <div>
+    <h1>Index</h1>
+  </div>
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    }
+  }
 }
